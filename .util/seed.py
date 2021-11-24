31e2c03c8cc6eb7b421c4fccbd9006efa8930048
baemako <bernard.schimmelpfennig@emako.pl>
2021-11-24 01:39:32 +0100

    added task 1 IDs randomization

--- .util/seed.py
+++ .util/seed.py
@@ -31,12 +31,24 @@ DATA_FILE = join(dirname(abspath(__file__)), "products.json")
 with open(DATA_FILE, "r") as file:
     data = load(file)
 
+ID_FILE = join(dirname(dirname(abspath(__file__))), "ids.csv")
+with open(ID_FILE, "w") as file:
+    for product in random.choices(data, k=30):
+        file.write(f"{product['id']}\n")
+
+
 for _ in range(5):
     for product in data:
         cursor = database.cursor()
         if product["type"] == "product":
+            modify = random.randint(0, 3)
             for variant_supply in product["details"]["supply"]:
                 for stock_data in variant_supply["stock_data"]:
+                    supply = stock_data["quantity"]
+                    if modify:
+                        supply = max(
+                            0, round(random.random() * supply) + random.randint(-5, 5)
+                        )
                     cursor.execute(
                         "INSERT INTO product_stocks "
                         "(time, product_id, variant_id, stock_id, supply) "
@@ -45,11 +57,7 @@ for _ in range(5):
                             product["id"],
                             variant_supply["variant_id"],
                             stock_data["stock_id"],
-                            max(
-                                0,
-                                round(random.random() * stock_data["quantity"])
-                                + random.randint(-5, 5),
-                            ),
+                            supply,
                         )
                     )
         database.commit()
