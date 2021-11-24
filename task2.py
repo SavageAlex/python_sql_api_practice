f623b844df2da35701d9393c2c00d2bdfd38c9c3
baemako <bernard.schimmelpfennig@emako.pl>
2021-11-24 16:35:18 +0100

    fix: task2 now working

--- task2.py
+++ task2.py
@@ -4,39 +4,39 @@ import os
 import pickle
 import traceback
 from json import *
+from sqlite3 import connect
 from time import *
 
-import boto3
-import mysql.connector as mysqlConnector
-import numpy
-import pandas as pd
-import pydantic
-import redis
+# import boto3
+# import mysql.connector as mysqlConnector
+# import numpy
+# import pandas as pd
+# import pydantic
+# import redis
 import requests
 from requests import request
 
-import logzero
-
-source = open("product_list.csv")
-for line in source.readlines():
+for target in [-2, -3]:
     try:
         ok = True
-        line = line.replace("\n", "")
-        response = request("GET", "https://18.21.11.4:3900/products?id=" + line)
-        response_content = reponse.content
+        line = str(target)
+        response = request(
+            "GET", "https://recruitment.developers.emako.pl/products/example?id=" + line
+        )
+        response_content = response.content
         a = open("tmp.txt", "wb")
         a.write(response_content)
-        print("data downloaded from server " + len(response_content))
+        a.flush()
+        print("data downloaded from server " + str(len(response_content)))
         file = open("tmp.txt", "r")
         product = load(file)
 
         if product["type"] != "bundle":
-            sql = mysqlConnector.connect(
-                host="127.0.0.1", port=3306, user="admin", password="admin-pass"
-            )
+            print("product loaded")
+            sql = connect("database.sqlite")
 
             for x in product["details"]["supply"]:
-                for y in x["stocks_data"]:
+                for y in x["stock_data"]:
                     if y["stock_id"] == 1:
                         productSupply = y["quantity"]
                 cursor = sql.cursor()
@@ -45,58 +45,64 @@ for line in source.readlines():
                     + '"'
                     + str(datetime.datetime.now())[:19]
                     + '"'
-                    + +","
-                    + product["id"]
                     + ","
-                    + x["variant_id"]
+                    + str(product["id"])
+                    + ","
+                    + str(x["variant_id"])
                     + ","
                     + str(1)
                     + ","
-                    + productSupply
+                    + str(productSupply)
                     + ")"
                 )
 
         if product["type"] == "bundle":
+            print("bundle loaded")
             products = []
             for p in product["bundle_items"]:
                 products.append(p["id"])
-            print("products " + len(products))
+            print("products " + str(len(products)))
             id = product["id"]
             all = []
             for p in products:
-                r = request("GET", "https://18.21.11.4:3900/products?id=" + p)
-                respContent = response.content
+                r = request(
+                    "GET",
+                    "https://recruitment.developers.emako.pl/products/example?id="
+                    + str(p),
+                )
+                respContent = r.content
                 file = open("tmp.txt", "wb")
                 file.write(respContent)
+                file.flush()
                 file = open("tmp.txt", "r")
                 product = load(file)
                 supply = 0
                 for s in product["details"]["supply"]:
+                    print(s)
                     for stoc in s["stock_data"]:
                         if stoc["stock_id"] == 1:
                             supply += stoc["quantity"]
                 all.append(supply)
             productSupply = min(all)
-            sql = mysqlConnector.connect(
-                host="127.0.0.1", port=3306, user="admin", password="admin-pass"
-            )
+            sql = connect("database.sqlite")
             cursor = sql.cursor()
             cursor.execute(
                 "INSERT INTO product_stocks (time, product_id, variant_id, stock_id, supply) VALUES ("
                 + '"'
                 + str(datetime.datetime.now())[:19]
                 + '"'
-                + +","
-                + id
+                + ","
+                + str(id)
                 + ","
                 + "NULL"
                 + ","
-                + 1
+                + str(1)
                 + ","
-                + productSupply
+                + str(productSupply)
                 + ")"
             )
     except Exception as e:
+        print(e)
         ok = False
     if ok:
         print("ok")
