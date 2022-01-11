3e4dca62fde8b3aa1e80acd56ffed066c20ffce1
SavageAlex <atalaver@gmail.com>
2022-01-11 02:07:00 +0100

    completed task3

--- task3.py
+++ task3.py
@@ -17,29 +17,65 @@ class Connector:
             "Accept": "application/json",
         }
 
+    def get_paginated_list(self, l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+
     def request(self, method: str, path: str, data: dict = {}) -> dict:
         return request(
             method, f"{DOMAIN}/{path}", json=data, headers=self.headers()
         ).json()
 
     def get_products(self, ids: Optional[List[int]] = None) -> List[dict]:
-        return self.request("GET", "products", {"ids": ids})["result"]
+        pagination = 40
+        response_list = []
+        if ids:
+            paginated_list = list(self.get_paginated_list(ids, pagination))
+            for paginated_item in paginated_list:
+                response = self.request("GET", "products", {"ids": paginated_item})["result"]
+                response_list += response
+        return response_list
+
+    def pagination_g(self, req_data):
+        response = self.request("GET", "products", req_data)
+        response_list = response["result"]
+        page_count = response["page_count"]
+        if page_count != 0:
+            for page in range(1, page_count):
+                pagination_dict = {"pagination": {"index": page}}                
+                page_response_list = self.request("GET", "products", {**req_data, **pagination_dict})["result"]
+                response_list += page_response_list
+        return response_list
 
     def get_all_products_summary(self) -> List[dict]:
-        return self.request("GET", "products", {"detailed": False})["result"]
+        req_data = {"detailed": False}
+        response_list = self.pagination_g(req_data)
+        return response_list
 
     def get_new_products(self, newer_than: Optional[datetime] = None) -> List[dict]:
         if newer_than is None:
             newer_than = datetime.now() - timedelta(days=5)
-        return self.request(
-            "GET", "products", {"created_at": {"start": newer_than.isoformat()}}
-        )["result"]
+        req_data = {"created_at": {"start": newer_than.isoformat()}}
+        response_list = self.pagination_g(req_data)
+        return response_list
+
+    def pagination_p_u(self, data_list, method):
+        pagination = 20
+        response_list = []
+        paginated_list = list(self.get_paginated_list(data_list, pagination))
+        for paginated_item in paginated_list:
+            response = self.request(method, "products", {"products": paginated_item})["result"]
+            response_list += response
+        return response_list
 
     def add_products(self, products: List[dict]):
-        return self.request("POST", "products", {"products": products})["result"]
+        response_list = self.pagination_p_u(self, products, "POST")
+        return response_list
 
     def update_stocks(self, stocks: Dict[int, list]):
         current_data = self.get_products(list(stocks))
         for product_entry in current_data:
             product_entry["details"]["supply"] = stocks[product_entry["id"]]
-        return self.request("PUT", "products", {"products": current_data})
+        response_list = self.pagination_p_u(self, current_data, "PUT")
+        response_dict = {"result": response_list}
+        return response_dict
